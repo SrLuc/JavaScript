7ec935325e9f52c484b3d82d035e1a607dde47e6
Ruan Lucena <ruanlucenaramos@gmail.com>
2022-09-16 14:44:52 -0300

    exercicios de função com Js
    fazendo alguns exemplo aritiméticos com Js e usando função com parametros e retornos

---
+++ converterMilhasParaQuilometros.js
@@ -0,0 +1,12 @@
+
+
+// criar um console que conveter milhas em km
+
+// milha 1.6 km
+
+function conveterMilhaKilometro(kilometro){
+    const milha = kilometro * 1.6;
+    return milha;
+}
+
+console.log("Suas milhas foram convertidas em " + conveterMilhaKilometro(5) + " Kilometros");
---
+++ informaçãoDeLogin.js
@@ -0,0 +1,12 @@
+
+// função para retornar o nome do usuario + informações de login
+
+function emailDoUsuario(nome){
+    const informação = nome + "@javascript.com"
+    return informação;
+}
+
+console.log(emailDoUsuario("ruanlucenaramos"));
+console.log(emailDoUsuario("pablomarroquino"));
+console.log(emailDoUsuario("marcosferreira"));
+console.log(emailDoUsuario("intensivo"));
