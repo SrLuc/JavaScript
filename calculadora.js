01ab1e1ba026028d74fcf641d8518f8aba815767
Ruan Lucena <ruanlucenaramos@gmail.com>
2022-09-18 20:55:27 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+//calculadora em javascript
+
+function calcular(numero1, numero2) {
+  var n1 = 0,
+    n2 = 0;
+  var result;
+  for (let i = 0; i < 10; i++) {
+    n1++, n2++;
+    result = n1 * n2;
+    console.log(n1 + "X" + n2 + "=" + result);
+  }
+}
+
+calcular();
